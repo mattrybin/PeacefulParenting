51e1171c35befe9bc054b0f7db88064e2f3de336
Matt Rybin <contact@mattrybin.com>
2023-12-07 06:24:12 +0100

    save: update

--- admin/src/App.tsx
+++ admin/src/App.tsx
@@ -18,38 +18,6 @@ import { Card, CardContent } from "@mui/material";
 import MailIcon from "@mui/icons-material/MailOutline";
 import CategoryIcon from "@mui/icons-material/LocalOffer";
 
-export const items = [
-  { id: "infant", icon: "egg-crack", title: "Infant", subtitle: "0-1 Years" },
-  { id: "toddler", icon: "baby", title: "Toddler", subtitle: "1-3 Years" },
-  { id: "child", icon: "tooth", title: "Child", subtitle: "3-8 Years" },
-  { id: "preteen", icon: "backpack", title: "Preteen", subtitle: "9-12 Years" },
-  {
-    id: "teen",
-    icon: "person-simple-throw",
-    title: "Teen",
-    subtitle: "13-16 Years",
-  },
-  {
-    id: "adult",
-    icon: "graduation-cap",
-    title: "Adult",
-    subtitle: "17-21 Years",
-  },
-  {
-    id: "household",
-    icon: "house-line",
-    title: "Household",
-    subtitle: "Parents & kids",
-  },
-  {
-    id: "relatives",
-    icon: "users-three",
-    title: "Relatives",
-    subtitle: "Beyond home",
-  },
-  { id: "other", icon: "users-three", title: "Other", subtitle: "" },
-];
-
 export const PostFilterSidebar = () => (
   <Card sx={{ order: -1, mr: 2, mt: 9, width: 200 }}>
     <CardContent>
