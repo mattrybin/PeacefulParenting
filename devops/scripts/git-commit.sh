813f7fe85b3561be49a0e75064df3d028b140f46
Matt Rybin <contact@mattrybin.com>
2024-01-14 10:32:08 +0100

    scripts/git-commit.sh changed with 34 lines changed

--- devops/scripts/git-commit.sh
+++ devops/scripts/git-commit.sh
@@ -11,5 +11,37 @@ function exit_if_development_branch {
     fi
 }
 
+function review_select_option {
+    PR_NUMBER=$(gh pr view --json number --jq .number)
+
+    while true; do
+        echo "Select an option:"
+        echo "0 - Quit"
+        echo "1 - Ask for review"
+        read -p "Your option: " option
+
+        case $option in
+            0 )
+                echo "Exiting..."
+                exit 0;;
+            1 )
+                echo "Asking for review..."
+                
+                # Makes the current PR ready for review
+                gh pr edit "$PR_NUMBER" --repo origin --ready-for-review
+                
+                # Adding "REVIEW" label to this PR
+                gh pr update "$PR_NUMBER" --add-label "REVIEW" --repo origin
+                
+                # Removing "DRAFT" label from this PR
+                gh pr update "$PR_NUMBER" --remove-label "DRAFT" --repo origin
+                break;;
+            * )
+                echo "Invalid option. Enter 0 or 1.";;
+        esac
+    done
+}
+
 exit_if_development_branch
 validate_working_directory
+review_select_option
