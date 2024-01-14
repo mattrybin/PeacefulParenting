7f69bfbe87bd568c0a998b1374792bdd2674ba5b
Matt Rybin <contact@mattrybin.com>
2024-01-14 13:44:49 +0100

    scripts/git-commit.sh changed with 62 lines changed

--- devops/scripts/git-commit.sh
+++ devops/scripts/git-commit.sh
@@ -1,37 +1,37 @@
 #!/bin/bash
 
 source /Users/mattrybin/SOFTWARE/mattrybin/PeacefulParenting/devops/scripts/utils/validate_working_directory.sh
-source /Users/mattrybin/SOFTWARE/mattrybin/PeacefulParenting/.env.local
+source /Users/mattrybin/SOFTWARE/mattrybin/PeacefulParenting/devops/scripts/utils/send_to_slack_review.sh
 
-function send_to_slack {
-    local pr_link=$1
-    local pr_title="$2"
-    local data='{
-        "blocks": [
-            {
-                "type": "section",
-                "text": {
-                    "type": "mrkdwn",
-                    "text": "*<'"$pr_link"'|'"$pr_title"'>*"
-                }
-            },
-            {
-                "type": "actions",
-                "elements": [
-                    {
-                        "type": "button",
-                        "text": { 
-                            "type": "plain_text", 
-                            "text": "Ready for Review" 
-                        }, 
-                        "url": "'"$pr_link"'"
-                    }
-                ]
-            }
-        ]
-    }'
-    curl -X POST -H 'Content-type: application/json' --data "${data}" $SLACK_WEBHOOK_URL
-}
+# function send_to_slack {
+#     local pr_link=$1
+#     local pr_title="$2"
+#     local data='{
+#         "blocks": [
+#             {
+#                 "type": "section",
+#                 "text": {
+#                     "type": "mrkdwn",
+#                     "text": "*<'"$pr_link"'|'"$pr_title"'>*"
+#                 }
+#             },
+#             {
+#                 "type": "actions",
+#                 "elements": [
+#                     {
+#                         "type": "button",
+#                         "text": { 
+#                             "type": "plain_text", 
+#                             "text": "Ready for Review" 
+#                         }, 
+#                         "url": "'"$pr_link"'"
+#                     }
+#                 ]
+#             }
+#         ]
+#     }'
+#     curl -X POST -H 'Content-type: application/json' --data "${data}" $SLACK_WEBHOOK_URL
+# }
 
 
 # Usage
---
+++ devops/scripts/utils/send_to_slack_review.sh
@@ -0,0 +1,31 @@
+
+source ../../.env.local
+function send_to_slack {
+    local pr_link=$1
+    local pr_title="$2"
+    local data='{
+        "blocks": [
+            {
+                "type": "section",
+                "text": {
+                    "type": "mrkdwn",
+                    "text": "*<'"$pr_link"'|'"$pr_title"'>*"
+                }
+            },
+            {
+                "type": "actions",
+                "elements": [
+                    {
+                        "type": "button",
+                        "text": { 
+                            "type": "plain_text", 
+                            "text": "Ready for Review" 
+                        }, 
+                        "url": "'"$pr_link"'"
+                    }
+                ]
+            }
+        ]
+    }'
+    curl -X POST -H 'Content-type: application/json' --data "${data}" $SLACK_WEBHOOK_URL
+}
