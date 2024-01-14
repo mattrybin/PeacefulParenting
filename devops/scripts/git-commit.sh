40cbeedc92b53ce03cd83815d48a5be959f876a9
Matt Rybin <contact@mattrybin.com>
2024-01-14 09:56:00 +0100

    Biggest scripts/git-merge-pr.sh | 2 files | 21 lines

--- devops/scripts/git-commit.sh
+++ devops/scripts/git-commit.sh
@@ -2,4 +2,14 @@
 
 source /Users/mattrybin/SOFTWARE/mattrybin/PeacefulParenting/devops/scripts/utils/validate_working_directory.sh
 
+function exit_if_development_branch {
+    current_branch=$(git branch --show-current)
+
+    if [ "$current_branch" == "development" ]; then
+        echo "You're on the 'development' branch. Exiting the script."
+        exit 1
+    fi
+}
+
+exit_if_development_branch
 validate_working_directory
