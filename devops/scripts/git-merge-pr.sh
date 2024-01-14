11194dd094b3344437e589024e10b87bc6ed1637
Matt Rybin <contact@mattrybin.com>
2024-01-14 05:14:38 +0100

    save: devops/scripts/git-merge-pr.sh and 1 other files changed with a total of 86 lines changed

--- devops/scripts/git-merge-pr.sh
+++ devops/scripts/git-merge-pr.sh
@@ -1,45 +1,6 @@
 #!/bin/bash
 
-source utils/validate_current_branch_up_to_date.sh
+pwd
+source /Users/mattrybin/SOFTWARE/mattrybin/PeacefulParenting/devops/scripts/utils/validate_working_directory.sh
 
-validate_current_branch_up_to_date
-# 
-# function validate_current_branch_up_to_date {
-#     # Update remote references
-#     git remote update
-    
-#     # Configure upstream
-#     current_branch=$(git branch --show-current)
-#     UPSTREAM=${current_branch:-'@{u}'}
-    
-#     LOCAL=$(git rev-parse @)
-#     REMOTE=$(git rev-parse "$UPSTREAM")
-#     BASE=$(git merge-base @ "$UPSTREAM")
-
-#     if [ $LOCAL != $REMOTE ]; then
-#         echo "${BOLD}Your current branch '$current_branch' is not up-to-date with the remote branch.${NORMAL}"
-        
-#         while true; do
-#             echo "Select an option:"
-#             echo "1 - Quit / Do nothing."
-#             echo "2 - Pull latest changes."
-#             read -p "Your option: " option
-
-#             case $option in
-#                 1 )
-#                     echo "Exiting without pulling changes..."
-#                     exit 1;;
-#                 2 )
-#                     git pull --rebase
-#                     if [ $? -ne 0 ]; then
-#                         echo "Error pulling latest changes. Please ensure no merge conflicts or unstaged changes are present."
-#                     else
-#                         echo "Successfully pulled latest changes."
-#                     fi
-#                     break;;
-#                 * )
-#                     echo "Invalid option. Enter 1 or 2.";;
-#             esac
-#         done
-#     fi
-# }
+validate_working_directory
---
+++ devops/scripts/utils/validate_working_directory.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+function list_changed_files {
+    # Get file with highest number of changes
+    full_path=$(git diff --numstat | awk '{print $1+$2, $3}' | sort -nr | awk 'NR==1{print $2}')
+    
+    # Limit to two directories back
+    first_file=$(echo $full_path | awk -F"/" '{OFS="/"; print $(NF-2),$(NF-1),$NF}')
+    
+    # Check the length of the file path, if it's too long (e.g., longer than 50 characters), limit to one directory back
+    if (( ${#first_file} > 50 )); then
+        first_file=$(echo $full_path | awk -F"/" '{OFS="/"; print $(NF-1),$NF}') 
+    fi
+
+    # Get array of changed files
+    changed_files_arr=($(git diff --name-only))
+
+    # Get length of array
+    arr_length=${#changed_files_arr[@]}
+
+    # Get total lines changed
+    total_lines_changed=$(git diff --numstat | awk '{ total += $1 + $2 } END { print total }')
+    
+    # Check if multiple files have been changed
+    if (( arr_length > 1 )); then
+       other_files_count=$((arr_length-1))
+       echo "save: ${first_file} and ${other_files_count} other files changed with a total of ${total_lines_changed} lines changed"
+    else
+       # Only one file has been changed
+       echo "save: ${first_file} changed with ${total_lines_changed} lines changed"
+    fi
+}
+
+function validate_working_directory {
+    commit_message=$(list_changed_files)
+    # Check if there are changes in working directory or changes in the index
+    if [[ "$(git status --porcelain)" != "" ]]; then
+        current_branch=$(git symbolic-ref --short HEAD)
+        echo "${BOLD}Current branch '${current_branch}' has uncommitted changes.${NORMAL}"
+        
+        while true; do
+            echo "Select an option:"
+            echo "1 - Quit / Do nothing."
+            echo "2 - Commit and push changes with a standard commit."
+            echo "3 - Stash changes."
+            echo "4 - Hard reset the branch to remote version."
+            read -p "Your option: " option
+
+            case $option in
+                1 )
+                    echo "Exiting without performing any operations..."
+                    exit 1;;
+                2 )
+                    git add --all && git commit -m "${commit_message}" && git push origin "$current_branch"
+                    if [ $? -ne 0 ]; then
+                        echo "Error while committing/pushing changes."
+                    else
+                        echo "Changes have been committed and pushed successfully."
+                    fi
+                    break;;
+                3 )
+                    git stash push -u -m "${current_branch}"
+                    if [ $? -ne 0 ]; then
+                        echo "Error stashing changes."
+                    else
+                        echo "Changes have been stashed successfully."
+                    fi
+                    break;;
+                4 )
+                    git fetch origin
+                    git reset --hard origin/"$current_branch"
+                    if [ $? -ne 0 ]; then
+                        echo "Error resetting the branch."
+                    else
+                        echo "Branch has been reset to the remote version successfully."
+                    fi
+                    break;;
+                * )
+                    echo "Invalid option. Enter 1, 2, 3 or 4.";;
+            esac
+        done
+    fi
+}
