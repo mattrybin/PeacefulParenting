98e948b472ad64be35ba4676bd77b643bbcc8fc2
Matt Rybin <workmateuszrybin@gmail.com>
2024-01-14 06:05:32 +0100

    feat: create footer (#5)
    * feat: create footer
    
    * save: PP-3
    
    * save: data.json and 2 other files changed with a total of 301 lines changed
    
    * save: devops/scripts/git-checkout-pr.sh changed with 1 lines changed
    
    * save: devops/scripts/git-checkout-pr.sh changed with 19 lines changed
    
    * save: (ResourcesFlow)/resources/page.tsx changed with 2 lines changed
    
    * save: devops/scripts/git-checkout-pr.sh changed with 54 lines changed
    
    * save: devops/scripts/git-checkout-pr.sh changed with 3 lines changed
    
    * save: devops/scripts/git-checkout-pr.sh changed with 43 lines changed
    
    * save: devops/scripts/git-checkout-pr.sh and 1 other files changed with a total of 105 lines changed
    
    * save: devops/scripts/git-merge-pr.sh and 1 other files changed with a total of 86 lines changed
    
    * save: scripts/utils/validate_working_directory.sh changed with 1 lines changed
    
    * save: scripts/utils/validate_working_directory.sh changed with 1 lines changed

---
+++ devops/scripts/git-merge-pr.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+pwd
+source /Users/mattrybin/SOFTWARE/mattrybin/PeacefulParenting/devops/scripts/utils/validate_working_directory.sh
+
+validate_working_directory
--- devops/scripts/utils/validate_working_directory.sh
+++ devops/scripts/utils/validate_working_directory.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-
 function list_changed_files {
     # Get file with highest number of changes
     full_path=$(git diff --numstat | awk '{print $1+$2, $3}' | sort -nr | awk 'NR==1{print $2}')
@@ -33,9 +32,9 @@ function list_changed_files {
 
 function validate_working_directory {
     commit_message=$(list_changed_files)
-    # Check if there are changes in working directory or changes in the index
     if [[ "$(git status --porcelain)" != "" ]]; then
         current_branch=$(git symbolic-ref --short HEAD)
+        echo $current_branch
         echo "${BOLD}Current branch '${current_branch}' has uncommitted changes.${NORMAL}"
         
         while true; do
@@ -81,100 +80,3 @@ function validate_working_directory {
         done
     fi
 }
-
-function checkout_pr {
-  # Obtain the current PR number
-  current_pr_number=$(gh pr view $(git branch --show-current) --json number --jq '.number')
-
-  # Fetch the PRs
-  prs=$(gh pr list --limit 100 --json number,title --jq '.[] | "\(.number) \(.title)"')
-
-  # Create an array of PR titles
-  options=()
-  prtitles=()
-
-  while IFS=" " read -r number title; do
-    if [ "$number" != "$current_pr_number" ]; then
-      options+=("$number")
-      prtitles+=("$title")
-    fi
-  done <<< "$prs"
-
-  # Display the options and prompt for a selection
-  prompt="Please select a PR: "
-  counter=0
-  echo "[$counter] Quit" # Add Quit option here
-  ((counter++)) # Increment counter
-  
-  for title in "${prtitles[@]}"; do 
-    echo "[$counter] $title" # Print title directly
-    ((counter++))
-  done
-
-  # Prompt for selection
-  read -p "$prompt" selection
-
-  # Quit option selected
-  if [[ $selection -eq 0 ]]; then
-    echo "Quitting..."
-    exit 0
-  fi
-
-  # Ensure 1 is subtracted for non-zero selection because array indices start from 0
-  selected_option=${options[$selection-1]}
-
-  if [[ $selected_option =~ ^[0-9]+ ]]; then
-    # The selected option starts with a number, so it's a PR
-    pr_number=${selected_option}
-    echo "Checking out PR $pr_number..."
-    gh pr checkout $pr_number
-    echo "Checked out PR $pr_number!"
-  else
-    echo "Invalid option. Please try again."
-  fi
-}
-
-function validate_current_branch_up_to_date {
-    # Update remote references
-    git remote update
-    
-    # Configure upstream
-    current_branch=$(git branch --show-current)
-    UPSTREAM=${current_branch:-'@{u}'}
-    
-    LOCAL=$(git rev-parse @)
-    REMOTE=$(git rev-parse "$UPSTREAM")
-    BASE=$(git merge-base @ "$UPSTREAM")
-
-    if [ $LOCAL != $REMOTE ]; then
-        echo "${BOLD}Your current branch '$current_branch' is not up-to-date with the remote branch.${NORMAL}"
-        
-        while true; do
-            echo "Select an option:"
-            echo "1 - Quit / Do nothing."
-            echo "2 - Pull latest changes."
-            read -p "Your option: " option
-
-            case $option in
-                1 )
-                    echo "Exiting without pulling changes..."
-                    exit 1;;
-                2 )
-                    git pull --rebase
-                    if [ $? -ne 0 ]; then
-                        echo "Error pulling latest changes. Please ensure no merge conflicts or unstaged changes are present."
-                    else
-                        echo "Successfully pulled latest changes."
-                    fi
-                    break;;
-                * )
-                    echo "Invalid option. Enter 1 or 2.";;
-            esac
-        done
-    fi
-}
-
-
-validate_working_directory
-checkout_pr
-validate_current_branch_up_to_date
