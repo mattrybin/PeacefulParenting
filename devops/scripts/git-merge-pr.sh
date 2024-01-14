fd130cbb79d95a27372ed46ab2581067d331dd9b
Matt Rybin <contact@mattrybin.com>
2024-01-14 08:55:23 +0100

    scripts/git-merge-pr.sh changed with 13 lines changed

--- devops/scripts/git-merge-pr.sh
+++ devops/scripts/git-merge-pr.sh
@@ -6,29 +6,26 @@ source /Users/mattrybin/SOFTWARE/mattrybin/PeacefulParenting/devops/scripts/util
 function check_current_pr_status {
     current_branch=$(git branch --show-current)
 
-echo $current_branch
     if [[ -z "$current_branch" ]]; then
         echo "No branch currently checked out."
         return 1
     fi
 
-    pr_number=$(gh pr list --base "$current_branch" --json number --jq '.[0].number')
+    pr_number=$(gh pr list --head "$current_branch" --json number --jq '.[0].number')
 
     if [[ -z "$pr_number" ]]; then
         echo "No Pull Request associated with the current branch."
         return 1
     fi
 
-echo $pr_number
-
     # Fetch Status
-    pr_status=$(gh api -X GET repos/:owner/:repo/commits/$current_branch/check-runs --jq '.check_runs[] | .conclusion' | sort | uniq)
+    pr_status=$(gh pr view "$pr_number" --json statusCheckRollup --jq '.statusCheckRollup.state')
 
-    echo $pr_status
+    echo "Checks Status: $pr_status"
 
-    if [[ $pr_status =~ "failure" ]]; then
+    if [[ $pr_status == "FAILURE" ]]; then
         echo "PR #$pr_number on '$current_branch' has failed checks."
-    elif [[ $pr_status == "success" ]]; then
+    elif [[ $pr_status == "SUCCESS" ]]; then
         echo "PR #$pr_number on '$current_branch' has passed checks."
     else
         echo "PR #$pr_number on '$current_branch' checks haven't finished yet or no checks are defined."
