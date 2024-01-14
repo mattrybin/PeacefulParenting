c5e3c86dcdc605815f7b1fb9cc5ec2c1f6b2ecdf
Matt Rybin <contact@mattrybin.com>
2024-01-14 08:56:18 +0100

    scripts/git-merge-pr.sh changed with 19 lines changed

--- devops/scripts/git-merge-pr.sh
+++ devops/scripts/git-merge-pr.sh
@@ -11,24 +11,23 @@ function check_current_pr_status {
         return 1
     fi
 
-    pr_number=$(gh pr list --head "$current_branch" --json number --jq '.[0].number')
+    pr=$(gh pr list --head "$current_branch" --json url --jq '.[0].url')
 
-    if [[ -z "$pr_number" ]]; then
+    if [[ -z "$pr" ]]; then
         echo "No Pull Request associated with the current branch."
         return 1
     fi
 
-    # Fetch Status
-    pr_status=$(gh pr view "$pr_number" --json statusCheckRollup --jq '.statusCheckRollup.state')
+    status=$(gh pr view --json checks -q 'checks(state)' $pr)
 
-    echo "Checks Status: $pr_status"
+    echo "Checks Status: $status"
 
-    if [[ $pr_status == "FAILURE" ]]; then
-        echo "PR #$pr_number on '$current_branch' has failed checks."
-    elif [[ $pr_status == "SUCCESS" ]]; then
-        echo "PR #$pr_number on '$current_branch' has passed checks."
+    if [[ $status == *"FAIL"* ]]; then
+        echo "PR for '$current_branch' has failed checks."
+    elif [[ $status == *"PASS"* ]]; then
+        echo "PR for '$current_branch' has passed checks."
     else
-        echo "PR #$pr_number on '$current_branch' checks haven't finished yet or no checks are defined."
+        echo "PR for '$current_branch' checks haven't finished yet or no checks are defined."
     fi
 }
 
