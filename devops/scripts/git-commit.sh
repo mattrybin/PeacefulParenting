422ebd513f2542561faa2e64e82901e697949035
Matt Rybin <contact@mattrybin.com>
2024-01-15 08:32:12 +0100

    scripts/git-commit.sh changed with 49 lines changed

--- devops/scripts/git-commit.sh
+++ devops/scripts/git-commit.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-source /Users/mattrybin/SOFTWARE/mattrybin/PeacefulParenting/devops/scripts/utils/validate_working_directory.sh
-source /Users/mattrybin/SOFTWARE/mattrybin/PeacefulParenting/devops/scripts/utils/send_to_slack_review.sh
 source ./.env.local
+
+SCRIPT_DIR=$(dirname "$BASH_SOURCE")
+source "$SCRIPT_DIR/utils/validate_working_directory.sh"
+source "$SCRIPT_DIR/utils/send_to_slack_review.sh"
 
 function exit_if_development_branch {
     current_branch=$(git branch --show-current)
@@ -27,26 +29,26 @@ function review_select_option {
                 echo "Exiting..."
                 exit 0;;
             2 )
-                echo "Asking for review..."
-                PR_LINK="https://github.com/mattrybin/peacefulparenting/pull/$PR_NUMBER/files"
-                PR_TITLE=$(gh pr view $PR_NUMBER --json title --jq .title)
+                notion_move_ticket "Review"
+                github_assign_state
+                # echo "Asking for review..."
+                # PR_LINK="https://github.com/mattrybin/peacefulparenting/pull/$PR_NUMBER/files"
+                # PR_TITLE=$(gh pr view $PR_NUMBER --json title --jq .title)
 
-                send_to_slack $PR_LINK "$PR_TITLE" $SLACK_WEBHOOK_URL
+                # # Get PR current labels
+                # PR_LABELS=$(gh pr view $PR_NUMBER --json labels --jq '.labels[]?.name' | tr -d '"')
 
-                # Get PR current labels
-                PR_LABELS=$(gh pr view $PR_NUMBER --json labels --jq '.labels[]?.name' | tr -d '"')
+                # # Check if "DRAFT" label exists
+                # if [[ $PR_LABELS =~ "DRAFT" ]]; then
+                #     # Removes the "DRAFT" label from this PR
+                #     gh pr edit "$PR_NUMBER" --remove-label "DRAFT"
+                # fi
 
-                # Check if "DRAFT" label exists
-                if [[ $PR_LABELS =~ "DRAFT" ]]; then
-                    # Removes the "DRAFT" label from this PR
-                    gh pr edit "$PR_NUMBER" --remove-label "DRAFT"
-                fi
-
-                # Check if "REVIEW" label does not exist
-                if [[ ! $PR_LABELS =~ "REVIEW" ]]; then
-                    # Adding "REVIEW" label to this PR
-                    gh pr edit "$PR_NUMBER" --add-label "REVIEW"
-                fi
+                # # Check if "REVIEW" label does not exist
+                # if [[ ! $PR_LABELS =~ "REVIEW" ]]; then
+                #     # Adding "REVIEW" label to this PR
+                #     gh pr edit "$PR_NUMBER" --add-label "REVIEW"
+                # fi
                 
                 break;;
             * )
@@ -58,3 +60,4 @@ function review_select_option {
 exit_if_development_branch
 validate_working_directory
 review_select_option
+send_slack_review
