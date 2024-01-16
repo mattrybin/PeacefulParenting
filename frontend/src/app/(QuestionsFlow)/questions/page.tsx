42e2fd04fb311d8ad07bf309b3d4486459506ff7
Matt Rybin <contact@mattrybin.com>
2024-01-16 08:48:53 +0100

    Biggest pnpm-lock.yaml | 10 files | 488 lines

--- frontend/src/app/(QuestionsFlow)/questions/page.tsx
+++ frontend/src/app/(QuestionsFlow)/questions/page.tsx
@@ -2,29 +2,32 @@
 import { PageContainer } from "shared/components/Containers"
 // import { Pagination } from "../components/Pagination"
 // import { QuestionsHeader } from "../components/headers"
-import { List } from "../components/List"
+// import { List } from "../components/List"
+// import { unstable_noStore } from "next/cache"
+// import { cookies } from "next/headers"
+// import { useEffect, useState } from "react"
+// import { MyComponent } from "../components/Component"
 // import { Tabs } from "../components/Tabs"
 
-export default async function HomePage() {
-  const data = await fetch("http://127.0.0.1:4100/api/v1/questions", {
-    headers: {
-      "Content-Type": "application/json"
-    },
-    // data will revalidate every hour
-    next: { revalidate: 3600 }
-  })
-    .then(async (res) => {
-      return { count: res.headers.get("X-Total-Count"), response: await res.json() }
-    })
-    .catch((e) => console.log(e))
+async function getData() {
+  // const res = await fetch("https://dummy.restapiexample.com/api/v1/employees")
+  // const res = await fetch("http://backend.peacefulparenting.local/api/v1/questions")
+  const res = await fetch("http://backend:4100/api/v1/questions")
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error("Failed to fetch data")
+  }
+  return res.json()
+}
 
-  const headersList = new Headers()
-  const referer = headersList.get("X-Total-Count")
-  // console.log(data.response)
-  console.log(referer)
-  // const { header, tabs } = useQuestions()
+export default async function HomePage() {
+  const data = await getData()
   return (
     <PageContainer>
+      {/* <MyComponent /> */}
+      <div>{JSON.stringify(data)}</div>
       {/* <QuestionsHeader {...header} />
       <Tabs {...tabs} /> */}
       {/* <List {...list} /> */}
