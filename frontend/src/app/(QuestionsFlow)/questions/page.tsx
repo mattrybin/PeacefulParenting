0d29839f5993a4c78dcfdd77b9c97b9ab7fcfef9
Matt Rybin <contact@mattrybin.com>
2024-01-17 09:01:04 +0100

    Biggest questions/page.tsx | 3 files | 53 lines

--- frontend/src/app/(QuestionsFlow)/questions/page.tsx
+++ frontend/src/app/(QuestionsFlow)/questions/page.tsx
@@ -1,36 +1,20 @@
 // import { useQuestions } from "./page.hook"
+// import { getQuestions } from "app/testing"
 import { PageContainer } from "shared/components/Containers"
-// import { Pagination } from "../components/Pagination"
-// import { QuestionsHeader } from "../components/headers"
-// import { List } from "../components/List"
-// import { unstable_noStore } from "next/cache"
-// import { cookies } from "next/headers"
-// import { useEffect, useState } from "react"
-// import { MyComponent } from "../components/Component"
-// import { Tabs } from "../components/Tabs"
-
-async function getData() {
-  // const res = await fetch("https://dummy.restapiexample.com/api/v1/employees")
-  // const res = await fetch("http://backend.peacefulparenting.local/api/v1/questions")
-  const res = await fetch("http://backend:4100/api/v1/questions")
-  // The return value is *not* serialized
-  // You can return Date, Map, Set, etc.
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data")
-  }
-  return res.json()
-}
+import { getQuestions } from "shared/queries/questions"
+import { List } from "../components/List"
 
 export default async function HomePage() {
-  const data = await getData()
+  const { data, count } = await getQuestions({})
   return (
     <PageContainer>
       {/* <MyComponent /> */}
-      <div>{JSON.stringify(data)}</div>
       {/* <QuestionsHeader {...header} />
       <Tabs {...tabs} /> */}
-      {/* <List {...list} /> */}
+      <List
+        questions={data}
+        isEmpty={count === 0}
+      />
       {/* <Pagination {...pagination} /> */}
     </PageContainer>
   )
