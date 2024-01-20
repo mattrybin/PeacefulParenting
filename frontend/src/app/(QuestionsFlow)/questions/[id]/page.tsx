50d39fc46c58b4a63e1c2be32100b2c7b26b4b86
Nikita <ngkhua2004@gmail.com>
2024-01-20 13:12:13 +0100

    questions/[id]/page.tsx changed with 160 lines changed

--- frontend/src/app/(QuestionsFlow)/questions/[id]/page.tsx
+++ frontend/src/app/(QuestionsFlow)/questions/[id]/page.tsx
@@ -18,52 +18,53 @@ export default function HomePage() {
   const { title, category } = useQuestion();
   return (
     <PageContainer>
-      <div className="border-b">
-        <div className=" grid gap-2">
-          <div className="text-xl font-semibold">
-            <div className="p-6">
-              <div className="py-2 text-lg">
-                <i className="ph-bold ph-baby"></i> Toddler
+      <div className="text-base-content">
+        <div className="border-b">
+          <div className=" grid gap-2">
+            <div className="text-xl font-semibold">
+              <div className="p-6">
+                <div className="py-2 text-lg">
+                  <i className="ph-bold ph-baby"></i> Toddler
+                </div>
+                <div>{title}</div>
               </div>
-              <div>{title}</div>
-            </div>
-            <div className="flex">
-              <div className="w-max text-3xl border border-base-content border-l-0 content-center grid">
-                <div
-                  className="cursor-pointer p-2 px-6 hover:opacity-60 transition"
-                  onClick={() => console.log("UP")}
-                >
-                  <i className="ph-bold ph-arrow-circle-up"></i>
+              <div className="flex">
+                <div className="w-max text-3xl border border-base-content border-l-0 content-center grid">
+                  <div
+                    className="cursor-pointer p-2 px-6 hover:opacity-60 transition"
+                    onClick={() => console.log("UP")}
+                  >
+                    <i className="ph-bold ph-arrow-circle-up"></i>
+                  </div>
+                  <div className="text-xl flex justify-center py-1">23</div>
+                  <div
+                    className="cursor-pointer p-2 px-6 hover:opacity-60 transition"
+                    onClick={() => console.log("DOWN")}
+                  >
+                    <i className="ph-bold ph-arrow-circle-down"></i>
+                  </div>
                 </div>
-                <div className="text-xl flex justify-center py-1">23</div>
-                <div
-                  className="cursor-pointer p-2 px-6 hover:opacity-60 transition"
-                  onClick={() => console.log("DOWN")}
-                >
-                  <i className="ph-bold ph-arrow-circle-down"></i>
-                </div>
-              </div>
-              <div className="w-full">
-                <div className="flex p-4 gap-2 h-max w-full border border-base-content border-x-0">
-                  <div className="bg-base-300 h-6 w-6 rounded border border-base-300/35"></div>
-                  <div className="text-base">Matt Rybin</div>
-                </div>
-                <div className="flex justify-around text-base border border-r-0">
-                  <div className="p-4 mx-auto">4 Answers</div>
-                  <div className="bg-base-content w-[1px]"></div>
-                  <div className="p-4 mx-auto">123 Views</div>
-                </div>
-                <div className="p-4 border text-base border-r-0">
-                  <div className="flex gap-2 items-center">
-                    <i className="ph-bold ph-calendar text-7"></i>Create 24 days
-                    ago
+                <div className="w-full">
+                  <div className="flex p-4 gap-2 h-max w-full border border-base-content border-x-0">
+                    <div className="bg-base-300 h-6 w-6 rounded border border-base-300/35"></div>
+                    <div className="text-base">Matt Rybin</div>
+                  </div>
+                  <div className="flex justify-around text-base border border-r-0">
+                    <div className="p-4 mx-auto">4 Answers</div>
+                    <div className="bg-base-content w-[1px]"></div>
+                    <div className="p-4 mx-auto">123 Views</div>
+                  </div>
+                  <div className="p-4 border text-base border-x-0 border-base-300">
+                    <div className="flex gap-2 items-center">
+                      <i className="ph-bold ph-calendar text-7"></i>Create 24
+                      days ago
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        {/* <div className="grid grid-flow-col grid-cols-2 justify-between border-t text-center h-16">
+          {/* <div className="grid grid-flow-col grid-cols-2 justify-between border-t text-center h-16">
           <div className="border-r flex items-center text-left justify-center gap-2">
             <Icons variant={category.icon} weight={"duotone"} size="7" />
             <div className="leading-5">
@@ -75,38 +76,65 @@ export default function HomePage() {
             <div className="font-semibold">123 Views</div>
           </div>
         </div> */}
-      </div>
-      <div>
-        <Markdown className="px-6 py-6 prose">{markdown}</Markdown>
-      </div>
-      <div className="">
-        <div className="text-base font-semibold flex justify-center border border-base-content border-x-0 p-2">
-          <span>3 Comments</span>
         </div>
         <div>
-          <div className="">
-            <Comments
-              text="Eventually she started to bowl on an empty lane, and when the other children came over she was talking to them."
-              dateTime="May 9, 2023 at 10:23"
-              commentator="Dave Smith"
-            />
-            <Comments
-              text="Eventually she started to bowl on an empty lane, and when the other children came over she was talking to them."
-              dateTime="May 9, 2023 at 10:23"
-              commentator="Dave Smith"
-            />
-            <Comments
-              text="Eventually she started to bowl on an empty lane, and when the other children came over she was talking to them."
-              dateTime="May 9, 2023 at 10:23"
-              commentator="Dave Smith"
-            />
-          </div>
-
-          <div></div>
-          <div></div>
+          <Markdown className="px-6 py-6 prose">{markdown}</Markdown>
         </div>
-        <div>Add Comments</div>
-        <div>3 Answers</div>
+        <div className="">
+          <div className="text-base font-semibold flex justify-center border border-base-content border-x-0 p-2">
+            <span>3 Comments</span>
+          </div>
+          <div>
+            <div className="">
+              <Comments
+                text="Eventually she started to bowl on an empty lane, and when the other children came over she was talking to them."
+                dateTime="May 9, 2023 at 10:23"
+                commentator="Dave Smith"
+              />
+              <Comments
+                text="Eventually she started to bowl on an empty lane, and when the other children came over she was talking to them."
+                dateTime="May 9, 2023 at 10:23"
+                commentator="Dave Smith"
+              />
+              <Comments
+                text="Eventually she started to bowl on an empty lane, and when the other children came over she was talking to them."
+                dateTime="May 9, 2023 at 10:23"
+                commentator="Dave Smith"
+              />
+            </div>
+          </div>
+          <div className="text-sm font-semibold flex justify-center border-b-0 border border-base-content border-x-0 p-2.5 place-items-center gap-1">
+            <i className="ph-bold ph-plus-square text-base-content text-base"></i>
+            <span>Add Comments</span>
+          </div>
+          {/* I could'nt really get the perfect matching colour to the design here, sorry */}
+          <div className="bg-fuchsia-300 border-fuchsia-700 text-base font-semibold flex justify-center border-2 border-x-0 p-2 place-items-center gap-1">
+            <span>3 Answers</span>
+          </div>
+          <div className="flex font-semibold">
+            <div className="w-max text-3xl border border-t-0 border-base-content border-l-0 content-center grid">
+              <div
+                className="cursor-pointer p-2 px-6 hover:opacity-60 transition"
+                onClick={() => console.log("UP")}
+              >
+                <i className="ph-bold ph-arrow-circle-up"></i>
+              </div>
+              <div className="text-xl flex justify-center py-1">23</div>
+              <div
+                className="cursor-pointer p-2 px-6 hover:opacity-60 transition"
+                onClick={() => console.log("DOWN")}
+              >
+                <i className="ph-bold ph-arrow-circle-down"></i>
+              </div>
+            </div>
+            <div className="w-full">
+              <div className="flex p-4 gap-2 border border-t-0 border-base-content border-x-0">
+                <div className="bg-base-300 h-6 w-6 rounded border border-base-300/35"></div>
+                <div className="text-base">Matt Rybin</div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </PageContainer>
   );
