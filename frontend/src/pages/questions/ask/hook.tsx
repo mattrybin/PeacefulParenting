27318619f861f1f5b9354fe6d79e6d08e33bb362
NikitaHukov <154896947+NikitaHukov@users.noreply.github.com>
2024-02-07 06:43:56 +0100

    feat: redesign single page (#26)
    * feat: redesign single page
    
    * questions/[id]/page.tsx changed with 160 lines changed
    
    * Biggest resources/page.data.ts | 6 files | 290 lines
    
    ---------
    
    Co-authored-by: Matt Rybin <contact@mattrybin.com>

--- frontend/src/pages/questions/ask/hook.tsx
+++ frontend/src/pages/questions/ask/hook.tsx
@@ -1,18 +1,21 @@
 // import { useQuestions } from "./page.hook"
 // import { getQuestions } from "app/testing"
-import { PageContainer } from "shared/components/Containers";
-import { getQuestions } from "shared/queries/questions";
-import { List } from "../components/List";
-const array = [{ id: "2342" }];
+import { PageContainer } from "shared/components/Containers"
+import { getQuestions } from "shared/queries/questions"
+import { List } from "../components/List"
+const array = [{ id: "2342" }]
 export default async function HomePage() {
-  const { data, count } = await getQuestions({});
+  const { data, count } = await getQuestions({})
   return (
     <PageContainer>
       {/* <MyComponent /> */}
       {/* <QuestionsHeader {...header} />
       // <Tabs {...tabs} /> */}
-      <List questions={array} isEmpty={count === 0} />
+      <List
+        questions={array}
+        isEmpty={count === 0}
+      />
       {/* <Pagination {...pagination} /> */}
     </PageContainer>
-  );
+  )
 }
