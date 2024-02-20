e4881a0c21d72249ec3e7aa2310e3378fae99ab1
NikitaHukov <154896947+NikitaHukov@users.noreply.github.com>
2024-02-20 13:13:46 +0100

    chore: resources page (#47)

---
+++ frontend/src/pages/resources/index.page.tsx
@@ -0,0 +1,35 @@
+import { Book } from "./components/Books";
+import { PageContainer, SectionContainer } from "./components/Containers";
+import { Course } from "./components/Course";
+import { HeaderSection } from "./components/HeaderSection";
+import { YoutubeChannel } from "./components/YoutubeChannel";
+import { books, courses, youtubechannels } from "./index.data";
+
+export default function Page() {
+  return (
+    <PageContainer>
+      <SectionContainer>
+        <HeaderSection icon="book-open-text" title="Books" />
+        <div className="grid gap-4 desktop:grid-cols-2 desktop:gap-x-8">
+          {books.map((item, index) => (
+            <Book key={index} {...item} />
+          ))}
+        </div>
+      </SectionContainer>
+      <SectionContainer>
+        <HeaderSection icon="certificate" title="Courses" />
+        <div className="grid gap-5 max-content">
+          {courses.map((item, index) => (
+            <Course key={index} {...item} />
+          ))}
+        </div>
+      </SectionContainer>
+      <SectionContainer>
+        <HeaderSection icon="video" title="Youtube channels" />
+        {youtubechannels.map((item, index) => (
+          <YoutubeChannel key={index} {...item} />
+        ))}
+      </SectionContainer>
+    </PageContainer>
+  );
+}
