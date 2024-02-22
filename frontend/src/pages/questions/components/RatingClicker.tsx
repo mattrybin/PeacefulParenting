5adeac0bdde615a146dc894ee04c45b659684a11
Nikita <ngkhua2004@gmail.com>
2024-02-22 12:41:11 +0100

    Biggest components/RatingClicker.tsx | 2 files | 53 lines

--- frontend/src/pages/questions/components/RatingClicker.tsx
+++ frontend/src/pages/questions/components/RatingClicker.tsx
@@ -31,6 +31,41 @@ export const RatingClicker = () => {
 
 export const RatingClicker2 = () => {
   const [count, setCount] = useState(0);
+  const [IconWeight, SetIconWeight] = useState("bold");
+  const [IconWeight2, SetIconWeight2] = useState("bold");
+  const [buttonClicked, setButtonClicked] = useState(false);
+  const [buttonClicked2, setButtonClicked2] = useState(false);
+  const HandleLike = () => {
+    if (buttonClicked2 == true) {
+      setCount(count + 2);
+      SetIconWeight("fill");
+      setButtonClicked(true);
+      SetIconWeight2("bold");
+      setButtonClicked2(false);
+    } else if (!buttonClicked) {
+      setCount(count + 1);
+      SetIconWeight("fill");
+      setButtonClicked(true);
+      SetIconWeight2("bold");
+      setButtonClicked2(false);
+    }
+  };
+  const HandleDislike = () => {
+    if (buttonClicked == true) {
+      setCount(count - 2);
+      SetIconWeight("bold");
+      setButtonClicked(false);
+      SetIconWeight2("fill");
+      setButtonClicked2(true);
+    } else if (!buttonClicked2) {
+      setCount(count - 1);
+      SetIconWeight2("fill");
+      setButtonClicked2(true);
+      SetIconWeight("bold");
+      setButtonClicked(false);
+    }
+  };
+
   return (
     <div className="border-r border-base-content/20">
       <div className="flex justify-center h-14 ">
@@ -40,19 +75,19 @@ export const RatingClicker2 = () => {
       <div className="flex border-t border-base-content/20 h-14">
         <div
           className="grid place-content-center w-14 cursor-pointer hover:opacity-60 transition border-r border-base-content/20"
-          onClick={() => {
-            setCount(count + 1);
-          }}
+          onClick={HandleLike}
         >
-          <Icons variant="thumbs-up" weight="bold" size="5" />
+          <Icons variant="thumbs-up" weight={IconWeight as "bold"} size="5" />
         </div>
         <div
           className="grid place-content-center w-14 cursor-pointer hover:opacity-60 transition"
-          onClick={() => {
-            setCount(count - 1);
-          }}
+          onClick={HandleDislike}
         >
-          <Icons variant="thumbs-down" weight="bold" size="5" />
+          <Icons
+            variant="thumbs-down"
+            weight={IconWeight2 as "bold"}
+            size="5"
+          />
         </div>
       </div>
     </div>
