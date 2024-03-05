6c2a901e499dd851e557715e9055554a8033a380
Matt Rybin <contact@mattrybin.com>
2024-03-05 09:44:56 +0100

    components/RatingClicker.tsx changed with 90 lines changed

--- frontend/src/pages/questions/components/RatingClicker.tsx
+++ frontend/src/pages/questions/components/RatingClicker.tsx
@@ -1,70 +1,52 @@
-"use client";
-import { useState } from "react";
-import { Icons } from "shared/components/Icons";
+"use client"
+import { useState } from "react"
+import { Icons } from "shared/components/Icons"
 
 export const RatingClicker = () => {
-  const [count, setCount] = useState(0);
+  const [count, setCount] = useState(0)
   return (
     <div className="w-max text-2xl ipad:text-5xl content-center grid">
       <div
         className="cursor-pointer p-2 px-6 hover:opacity-60 transition"
         onClick={() => {
-          setCount(count + 1);
+          setCount(count + 1)
         }}
       >
         <i className="ph-bold ph-arrow-circle-up"></i>
       </div>
-      <div className="text-xl flex justify-center py-1 ipad:text-2xl">
-        {count}
-      </div>
+      <div className="text-xl flex justify-center py-1 ipad:text-2xl">{count}</div>
       <div
         className="cursor-pointer p-2 px-6 hover:opacity-60 transition"
         onClick={() => {
-          setCount(count - 1);
+          setCount(count - 1)
         }}
       >
         <i className="ph-bold ph-arrow-circle-down"></i>
       </div>
     </div>
-  );
-};
+  )
+}
 
 export const RatingClicker2 = () => {
-  const [count, setCount] = useState(0);
-  const [IconWeight, SetIconWeight] = useState("bold");
-  const [IconWeight2, SetIconWeight2] = useState("bold");
-  const [buttonClicked, setButtonClicked] = useState(false);
-  const [buttonClicked2, setButtonClicked2] = useState(false);
-  const HandleLike = () => {
-    if (buttonClicked2 == true) {
-      setCount(count + 2);
-      SetIconWeight("fill");
-      setButtonClicked(true);
-      SetIconWeight2("bold");
-      setButtonClicked2(false);
-    } else if (!buttonClicked) {
-      setCount(count + 1);
-      SetIconWeight("fill");
-      setButtonClicked(true);
-      SetIconWeight2("bold");
-      setButtonClicked2(false);
-    }
-  };
-  const HandleDislike = () => {
-    if (buttonClicked == true) {
-      setCount(count - 2);
-      SetIconWeight("bold");
-      setButtonClicked(false);
-      SetIconWeight2("fill");
-      setButtonClicked2(true);
-    } else if (!buttonClicked2) {
-      setCount(count - 1);
-      SetIconWeight2("fill");
-      setButtonClicked2(true);
-      SetIconWeight("bold");
-      setButtonClicked(false);
-    }
-  };
+  const [count, setCount] = useState(0)
+  const [upIconActive, setUpIconActive] = useState(false)
+  const [downIconActive, setDownIconActive] = useState(false)
+
+  const handleReset = () => {
+    setUpIconActive(false)
+    setDownIconActive(false)
+    setCount(0)
+  }
+  const handleLike = () => {
+    setUpIconActive(true)
+    setDownIconActive(false)
+    setCount(1)
+  }
+  const handleDislike = () => {
+    setUpIconActive(false)
+    setDownIconActive(true)
+    setCount(-1)
+  }
 
   return (
     <div className="border-r border-base-content/20">
@@ -75,21 +57,25 @@ export const RatingClicker2 = () => {
       <div className="flex border-t border-base-content/20 h-14">
         <div
           className="grid place-content-center w-14 cursor-pointer hover:opacity-60 transition border-r border-base-content/20"
-          onClick={HandleLike}
+          onClick={!upIconActive ? handleLike : handleReset}
         >
-          <Icons variant="thumbs-up" weight={IconWeight as "bold"} size="5" />
+          <Icons
+            variant="thumbs-up"
+            weight={upIconActive ? "fill" : "bold"}
+            size="5"
+          />
         </div>
         <div
           className="grid place-content-center w-14 cursor-pointer hover:opacity-60 transition"
-          onClick={HandleDislike}
+          onClick={!downIconActive ? handleDislike : handleReset}
         >
           <Icons
             variant="thumbs-down"
-            weight={IconWeight2 as "bold"}
+            weight={downIconActive ? "fill" : "bold"}
             size="5"
           />
         </div>
       </div>
     </div>
-  );
-};
+  )
+}
