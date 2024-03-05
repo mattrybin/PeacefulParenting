61954b254dba6a84ba5eac610ce289ea6441e6df
Matt Rybin <workmateuszrybin@gmail.com>
2024-03-05 10:09:41 +0100

    fix: small responsive attr (#53)

--- frontend/src/shared/components/Header.tsx
+++ frontend/src/shared/components/Header.tsx
@@ -1,11 +1,11 @@
-"use client";
+"use client"
 
-import "swiper/css";
+import "swiper/css"
 // import { HeaderController, MobileHeaderController } from "./Header/HeaderController"
-import { HeaderBranding } from "./Header/Branding";
-import { FilterTab } from "./Header/FilterTab";
-import { MobileHeaderController } from "./Header/HeaderController";
-import Link from "next/link";
+import { HeaderBranding } from "./Header/Branding"
+import { FilterTab } from "./Header/FilterTab"
+import { MobileHeaderController } from "./Header/HeaderController"
+import Link from "next/link"
 
 export const Header = () => (
   <div className="header border-b border-base-200 bg-base-100">
@@ -18,7 +18,7 @@ export const Header = () => (
     </div>
     <div className="hidden ipad:block">
       <div className="header border-b border-base-200 gap-2">
-        <div className="small:max-content desktop:mx-auto desktop:w-[1240px] h-16 grid grid-flow-col items-center grid-cols-[max-content_max-content] justify-between ap-10 px-4">
+        <div className="desktop:mx-auto desktop:w-[1240px] h-16 grid grid-flow-col items-center grid-cols-[max-content_max-content] justify-between ap-10 px-4">
           <HeaderBranding />
           {/* <Menu /> */}
           <MobileHeaderController />
@@ -27,18 +27,27 @@ export const Header = () => (
       </div>
     </div>
   </div>
-);
+)
 
 const Menu = () => (
   <div className="flex gap-2 justify-center">
-    <Link className="btn btn-ghost" href="/questions">
+    <Link
+      className="btn btn-ghost"
+      href="/questions"
+    >
       Questions
     </Link>
-    <Link className="btn btn-ghost" href="/chat">
+    <Link
+      className="btn btn-ghost"
+      href="/chat"
+    >
       Chat AI
     </Link>
-    <Link className="btn btn-ghost" href="/resources">
+    <Link
+      className="btn btn-ghost"
+      href="/resources"
+    >
       Resources
     </Link>
   </div>
-);
+)
